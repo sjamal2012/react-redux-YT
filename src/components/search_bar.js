7855f051ed0cc3c8c50d9d2ed3b7e5cd1d208c99
Sammy Jamal <sammyjamal12@gmail.com>
2018-06-19 22:16:50 -0700

    finishing React side of YT app

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -12,9 +12,14 @@ class SearchBar extends Component {
       <div className='search-bar'>
         <input
           value={this.state.term}
-          onChange={ event => this.setState({ term: event.target.value })} />
+          onChange={ event => this.onInputchange(event.target.value)} />
       </div>
     );
+  }
+
+  onInputchange(term) {
+    this.setState({term});
+    this.props.onSearchTermChange(term);
   }
 }
 
